ed363a28af5044442be617775131112bf7f0bf4e
emmanuel <you@example.com>
2023-09-18 08:30:27 -0700

    4-puts.c(

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-
-
 /**
 *Description: main Prints out a string to stdout.
 *Return: 0 if success.
@@ -8,5 +6,5 @@
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0); 
+return (0);
 }
