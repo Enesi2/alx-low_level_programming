ac76e3276cc43cd2c222136ffeef791e3fc262b5
emmanuel <you@example.com>
2023-09-23 09:56:34 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,81 +1,22 @@
 #include "main.h"
 
 /**
- * more_numbers - print more numbers
+ * more_numbers - prints numbers between 0 to 14
+ * 10 times.
+ * Return: no return.
  */
-
 void more_numbers(void)
 {
-	int i, j;
+	int i, ch;
 
-	for (i = 1; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-	for (j = 0; j <= 14; j++)
-	{
-	if (j >= 10)
-	_putchar('1');
-	_putchar (j % 10 + '0');
+		for (ch = 0; ch < 15; ch++)
+		{
+			if (ch >= 10)
+				_putchar((ch / 10) + 48);
+			_putchar((ch % 10) + 48);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
-	}
-}#include "main.h"
-
-/**
- *  * more_numbers - print more numbers
- *   */
-
-void more_numbers(void)
-{
-		int i, j;
-
-			for (i = 1; i <= 10; i++)
-			{
-					for (j = 0; j <= 14; j++)
-					{
-							if (j >= 10)
-									_putchar('1');
-								_putchar (j % 10 + '0');
-					}
-						_putchar('\n');
-			}
-}#include "main.h"
-
-/**
- *  * more_numbers - print more numbers
- *   */
-
-void more_numbers(void)
-{
-		int i, j;
-
-			for (i = 1; i <= 10; i++)
-			{
-					for (j = 0; j <= 14; j++)
-					{
-							if (j >= 10)
-									_putchar('1');
-								_putchar (j % 10 + '0');
-					}
-						_putchar('\n');
-			}
-}#include "main.h"
-
-/**
- *  * more_numbers - print more numbers
- *   */
-
-void more_numbers(void)
-{
-		int i, j;
-
-			for (i = 1; i <= 10; i++)
-			{
-					for (j = 0; j <= 14; j++)
-					{
-							if (j >= 10)
-									_putchar('1');
-								_putchar (j % 10 + '0');
-					}
-						_putchar('\n');
-			}
 }
