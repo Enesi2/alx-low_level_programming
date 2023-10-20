86e44278e659947ad765bf4031c193b58adc89bd
Enesi2 <emmymy2@gmail.com>
2023-10-19 20:11:37 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,24 +1,24 @@
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * print_list - Prints length and the string, then returns amount of nodes
- * @h: Pointer to a struct constant
- * Return: Returns amount of node
-**/
+ * print_list - prints list.
+ * @h: param list
+ * Return: data_structure
+ */
 
 size_t print_list(const list_t *h)
 {
-	unsigned int count = 0;
+	size_t l_node;
 
+	l_node = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[%u] %s\n", 0, "(nil)");
+			printf("[%d] %s\n", 0, "(nil)");
 		else
-			printf("[%u] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		count++;
+		l_node++;
 	}
-	return (count);
+	return (l_node);
 }
